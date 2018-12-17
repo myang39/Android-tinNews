c2c5600806544597751b94f8fb6dda252eb25fb2
Minkang Yang <minkangyang@gmail.com>
2018-12-17 11:40:45 -0500

    first commit

---
+++ app/src/main/java/com/laioffer/tinnews/WebViewActivity.java
@@ -0,0 +1,38 @@
+package com.laioffer.tinnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
